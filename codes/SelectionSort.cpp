4405c1af133b9db9b85824e94315d9dd00ee3c90
Goryalera <yurkova.valeriya@list.ru>
2025-10-31 19:00:04 +0300

    Update SelectionSort.cpp

---
+++ codes/SelectionSort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++) {
+    	cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+void selectionSort(int arr[], int n) {
+
+	for (int i = 0; i < n - 1; ++i) { 	
+    	int minIndex = i;
+    	for (int j = i + 1; j < n; ++j) {
+        	if (arr[j] < arr[minIndex]) {   
+            	minIndex = j;           	
+        	}
+    	}
+    	swap(arr[i], arr[minIndex]);   
+	}
+}
